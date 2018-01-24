bde09da2f43e25d7aa0d260e0f4321e5aa46c6fe
Pakdeepum <31123616+Pakdeepum@users.noreply.github.com>
2018-01-24 23:35:19 +0700

    Add files via upload

---
+++ src/Node.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package homework1;
+
+/**
+ *
+ * @author Dell
+ */
+public class Node {
+        Node left;
+        Node right;
+        char key;
+        public Node(char data) {
+            this.key = data;
+        }
+        public String toString() {
+        return Character.toString(key);
+    }
+}
+//
